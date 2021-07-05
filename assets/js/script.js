81571471cd96780bb6ab09394d910ac13369a236
seanparnold <sean.p.arnold@gmail.com>
2021-07-05 12:04:40 -0700

    Built out road map comments in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+/*Substantiate timer on page that corresponds to object with state variables */
+
+/*Substantiate quiz display corresponding to question generator object */
+
+/*Create function that progresses quiz based on accessible state variables */
+
+/*Substantiate object that handles score recording */
+
+/*Create function that passes data to local storage */
+
+/*Create function that dynamically switches between quiz and score displays */
